808899ba7e972e1a5aff773b01c9c61ef2499dec
Min <mijien0179@gmail.com>
2021-11-05 01:59:53 +0900

    생성 로직 정규화

--- routes/install.js
+++ routes/install.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('mysql');
 const sec = require("../Server/security");
-const si = require("systeminformation");
 const fs = require('fs');
 
 router.get('/', function (req, res, next) { // 설치 초기 페이지
@@ -14,56 +12,51 @@ router.get('/', function (req, res, next) { // 설치 초기 페이지
 
 });
 
-router.post('/', function (req, res, next) { // 설치에 필요한 데이터를 얻은 상태
-    var c = db.createConnection({
+router.post('/', async function (req, res, next) { // 설치에 필요한 데이터를 얻은 상태
+    
+    const si = require('systeminformation');
+    const body = {
         host: req.body.dbHost,
         user: req.body.dbId,
-        password: req.body.dbPassword,
-    });
-
-    let set = {
-        title: "TeamOSS",
-        set_comp: true,
-        result: "데이터베이스 접근 및 초기화 성공. 서버를 다시 시작하세요."
+        password: req.body.dbPassword
     };
-
-    si.baseboard(el =>{
-        const hash = sec.Hash(el.serial);
-        let conf = {
-            host:req.body.dbHost,
-            user:req.body.dbId,
-            password:sec.Encrypt(req.body.dbPassword, hash)
-        };
-        conf = JSON.stringify(conf);
-        fs.writeFile("./config.json", conf, function(err){});
-    });
-
+    const resResult = {
+        title:"TeamOSS",
+        set_comp:true,
+        result:"데이터베이스 초기화 성공. 서버를 다시 실행해주세요."
+    };
     
+    const mysql = require('mysql2/promise');
+    const connection = await mysql.createConnection(body)
+    await connection.query("CREATE DATABASE IF NOT EXISTS ??;", [req.body.dbName]);
+    await connection.query("USE ??;", req.body.dbName);
+    await connection.query('CREATE TABLE ??(idx int PRIMARY KEY, values_t int);', 'test');
     
-    c.connect(function (err) {
-        if (err) {
-            set.result = "데이터베이스 접근 오류. 다시 데이터베이스 계정을 다시 확인해보세요.";
-            set.set_comp = false;
-            res.render('install', set);
+    Promise.all([connection])
+    .then( _ =>{
+        si.baseboard()
+        .then(el=>{
+            body.database = req.body.dbName;
+            body.password = sec.Encrypt(body.password, sec.Hash(el.serial));
+
+            fs.writeFile("./config.json", JSON.stringify(body), _ =>{});
+
+        }).then(_ =>{
+            res.render('install', resResult);
             res.end();
-            return;
-        }
-        
-        c.query(`CREATE DATABASE IF NOT EXISTS ??;`, [req.body.dbName], function (err, result) {
-            if (err){
-                set.result = "데이터베이스 접근 오류. 다시 데이터베이스 계정을 다시 확인해보세요.";
-                set.set_comp = false;
-                res.render('install', set);
-                res.end();
-                return;
-            }
-            console.log("Create Database");
-            // TODO: 유저 데이터 테이블 등 서비스에 필요한 테이블 작성 필요
-
-            res.render('install', set);
+            process.exit();
+        }).catch((err) =>{
+            resResult.result = "설정 파일을 만드는 중 오류가 발생했습니다."
+            res.render('install', resResult);
+            res.end();
             
+        }).catch(err =>{
+            resResult.result = "데이터베이스 초기화 실패. 다시 작업하세요.";
+            res.render('install',resResult);
+            res.end();
         });
     });
+
 });
 
 module.exports = router;
