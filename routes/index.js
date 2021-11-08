123a861d8ae2d032482856535c88c4d5ab65b596
c0c0pang <coc1789@naver.com>
2021-11-08 23:29:43 +0900

    프로젝트 페이지 추가(테스트)

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,5 @@ router.get('/navigation', function(req, res, next) {
 router.get('/profile', function(req, res, next) {
   res.render('profile', { title: 'Express' });
 });
+
 module.exports = router;
