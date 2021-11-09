01c14a42dacdb28e69de205762b1969b1572b629
Min <mijien0179@gmail.com>
2021-11-09 22:10:31 +0900

    default router 추가

--- routes/_routerCtrl.js
+++ routes/_routerCtrl.js
@@ -7,4 +7,12 @@ var usersRouter = require("./users")
 router.use('/', indexRouter);
 router.use('/users', usersRouter);
 
+router.use((req,res) =>{
+    res.render('layout', {
+        content:'common/error',
+        css:[],
+        error:"지정되지 않은 페이지입니다.",
+    });
+});
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res, next) {
     }
     // res.render('index', { title: 'Express' });
     res.render('layout', {
-      content_file:"index",
+      content:"index",
       title:"Express",
       css:['style.css']
     })
