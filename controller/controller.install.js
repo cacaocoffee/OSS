67eed362cc2478ab67d1427123a07a8f9459d78a
Min <mijien0179@gmail.com>
2021-11-10 23:52:08 +0900

    Install 관련 버그 수정 installation 페이지에서 DB 접근 실패시 서버 프로그램이 죽는 문제 수정

--- controller/controller.install.js
+++ controller/controller.install.js
@@ -24,83 +24,88 @@ exports.InitializeDB = async (req, res, next) => {
         result:false,
         value:'데이터베이스 초기화 성공.<br>설치를 완료했습니다. 서버를 다시 실행해주세요.'
     };
-
-    const connection = await mysql.createConnection(body.db);
-    try {
-        await connection.beginTransaction();
-        await connection.query("CREATE DATABASE IF NOT EXISTS ??;", req.body.dbName);
-        await connection.query("USE ??;", req.body.dbName);
-        body.db.database = req.body.dbName;
-        /* TODO: 생성할 테이블 등 서비스 운영에 필요한 기본 테이블 등은 해당 주석 바로 아래 작성하면 됩니다. */
-        await connection.
-        query(`CREATE TABLE user (
-                id int unsigned NOT NULL AUTO_INCREMENT COMMENT '식별용 ID', 
-                userid varchar(16) NOT NULL COMMENT '로그인 ID',
-                pw char(64) NOT NULL COMMENT '로그인 PASSWORD',
-                name varchar(10) NOT NULL default 'unknown' COMMENT '사용자 이름',
-                authorize tinyint(1) NOT NULL default 0 COMMENT '페이지 이용 승인 여부',
-                PRIMARY KEY(id, userid)
-            );`);
-        await connection.
-        query(`CREATE TABLE language_list (
-            id int unsigned NOT NULL AUTO_INCREMENT COMMENT "언어 id",
-            language varchar(16),
-            PRIMARY KEY(id)
-            );`);       
-
-        await connection.
-        query(`CREATE TABLE language_user (
-                userid int unsigned NOT NULL AUTO_INCREMENT COMMENT 'user테이블 id', 
-                language int unsigned NOT NULL COMMENT '사용 언어',
-                PRIMARY KEY(userid,language),
-                FOREIGN KEY(userid) references user(id),
-                FOREIGN KEY(language) references language_list(id)
-            );`);
+    try{
+        const connection = await mysql.createConnection(body.db);
+        try {
+            await connection.beginTransaction();
+            await connection.query("CREATE DATABASE IF NOT EXISTS ??;", req.body.dbName);
+            await connection.query("USE ??;", req.body.dbName);
+            body.db.database = req.body.dbName;
+            /* TODO: 생성할 테이블 등 서비스 운영에 필요한 기본 테이블 등은 해당 주석 바로 아래 작성하면 됩니다. */
+            await connection.
+            query(`CREATE TABLE user (
+                    id int unsigned NOT NULL AUTO_INCREMENT COMMENT '식별용 ID', 
+                    userid varchar(16) NOT NULL COMMENT '로그인 ID',
+                    pw char(64) NOT NULL COMMENT '로그인 PASSWORD',
+                    name varchar(10) NOT NULL default 'unknown' COMMENT '사용자 이름',
+                    authorize tinyint(1) NOT NULL default 0 COMMENT '페이지 이용 승인 여부',
+                    PRIMARY KEY(id, userid)
+                );`);
+            await connection.
+            query(`CREATE TABLE language_list (
+                id int unsigned NOT NULL AUTO_INCREMENT COMMENT "언어 id",
+                language varchar(16),
+                PRIMARY KEY(id)
+                );`);       
+    
+            await connection.
+            query(`CREATE TABLE language_user (
+                    userid int unsigned NOT NULL AUTO_INCREMENT COMMENT 'user테이블 id', 
+                    language int unsigned NOT NULL COMMENT '사용 언어',
+                    PRIMARY KEY(userid,language),
+                    FOREIGN KEY(userid) references user(id),
+                    FOREIGN KEY(language) references language_list(id)
+                );`);
+                
+            const langList = [
+                'C/C++',
+                'JAVA',
+                'C#',
+                'Python'
+            ]
             
-        const langList = [
-            'C/C++',
-            'JAVA',
-            'C#',
-            'Python'
-        ]
-        
-        for(let i = 0; i < langList.length; ++i){
-            await connection.query(`INSERT INTO language_list (language) VALUES('${langList[i]}');`);
+            for(let i = 0; i < langList.length; ++i){
+                await connection.query(`INSERT INTO language_list (language) VALUES('${langList[i]}');`);
+            }
+            
+            await connection.commit();
+            Promise.all([connection])
+                .then((_) => {
+                    si.baseboard()
+                        .then((el) => {
+                            body.db.password = sec.Encrypt(body.db.password, sec.HashFor2Way(el.serial));
+                            body.serial = el.serial;
+    
+                            fs.writeFileSync("./config.json", JSON.stringify(body), (_) => { });
+                        })
+                        .then((_) => {
+                            resResult.result = true;
+                            res.render("install", resResult);
+                            res.end();
+                            connection.end();
+                        })
+                        .catch((err) => {
+                            resResult.value = `설정 파일을 만드는 중 오류가 발생했습니다. ${err}`;
+                            res.render("install", resResult);
+                            res.end();
+                        });
+                })
+                .catch((err) => {
+                    console.log(err);
+                    resResult.value = `데이터베이스 초기화 실패. 다시 작업하세요. ${err}`;
+                    res.render("install", resResult);
+                    res.end();
+                });
+        } catch (e) {
+            await connection.rollback();
+            resResult.value = `설치에 실패했습니다.<br>오류 내용: ${e}`;
+            res.render("install", resResult);
+            res.end();
         }
-        
-        await connection.commit();
-        Promise.all([connection])
-            .then((_) => {
-                si.baseboard()
-                    .then((el) => {
-                        body.db.password = sec.Encrypt(body.db.password, sec.HashFor2Way(el.serial));
-                        body.serial = el.serial;
-
-                        fs.writeFileSync("./config.json", JSON.stringify(body), (_) => { });
-                    })
-                    .then((_) => {
-                        resResult.result = true;
-                        res.render("install", resResult);
-                        res.end();
-                        connection.end();
-                    })
-                    .catch((err) => {
-                        resResult.value = `설정 파일을 만드는 중 오류가 발생했습니다. ${err}`;
-                        res.render("install", resResult);
-                        res.end();
-                    });
-            })
-            .catch((err) => {
-                console.log(err);
-                resResult.value = `데이터베이스 초기화 실패. 다시 작업하세요. ${err}`;
-                res.render("install", resResult);
-                res.end();
-            });
-    } catch (e) {
-        await connection.rollback();
-        resResult.value = `설치에 실패했습니다.<br>오류 내용: ${e}`;
+        await connection.end();
+    }catch(e){
+        resResult.value = `DB 접근에 실패했습니다. 패드워드가 다르지는 않는지 확인하세요.<br>오류 내용:${e}`;
         res.render("install", resResult);
         res.end();
     }
-    await connection.end();
 };
