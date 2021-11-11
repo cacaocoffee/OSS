44dc0403bd01b4ed280202ddbd2907c36194e3b1
c0c0pang <coc1789@naver.com>
2021-11-11 16:44:18 +0900

    프로젝트 페이지 추가

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,9 @@ router.get('/navigation', function(req, res, next) {
 router.get('/profile', function(req, res, next) {
   res.render('profile', { title: 'Express' });
 });
+router.get('/project', function (req, res, next) {
+  res.render('project', { title: 'Express' });
+});
 router.get('/project_main', function(req, res, next) {
   res.render('project_main', { title: 'Express' });
 });
